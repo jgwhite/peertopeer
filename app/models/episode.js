d214e35eb83fc19c7724f9fbbca39bd135ddc593
Jamie White <jamie@jgwhite.co.uk>
2014-11-24 16:28:50 +0000

    Implement prequels

--- app/models/episode.js
+++ app/models/episode.js
@@ -8,11 +8,19 @@ export default DS.Model.extend({
   guest: DS.belongsTo('partner', { inverse: 'guestEpisode' }),
   host: DS.belongsTo('partner', { inverse: 'hostEpisodes' }),
 
+  prequels: function() {
+    var host = this.get('host');
+
+    return this.store.filter('episode', function(episode) {
+      return episode.get('guest') === host;
+    });
+  }.property('host'),
+
   sequels: function() {
     var guest = this.get('guest');
 
-    return this.store.filter('episode', { host: true }, function(episode) {
+    return this.store.filter('episode', function(episode) {
       return episode.get('host') === guest;
     });
-  }.property()
+  }.property('guest')
 });

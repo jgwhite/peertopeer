1d4d090ac112736f6b240ce18a60eba8389e42d0
Jamie White <jamie@jgwhite.co.uk>
2014-11-24 16:52:58 +0000

    Filter down to root episodes
    - Also add (featuring ...)

--- app/models/partner.js
+++ app/models/partner.js
@@ -4,5 +4,9 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   guestEpisode: DS.belongsTo('episode', { inverse: 'guest' }),
-  hostEpisodes: DS.hasMany('episode', { inverse: 'host' })
+  hostEpisodes: DS.hasMany('episode', { inverse: 'host' }),
+
+  name: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName')
 });

d22aeb7e7496e1c4226a8f12c32ed9804f98ab11
Jamie White <jamie@jgwhite.co.uk>
2014-11-24 15:45:35 +0000

    Add missing attrs

--- app/models/episode.js
+++ app/models/episode.js
@@ -2,6 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   challengeName: DS.attr('string'),
+  guestName: DS.attr('string'),
+  hostName: DS.attr('string'),
+  posterFrameUrl: DS.attr('string'),
   guest: DS.belongsTo('partner', { inverse: 'guestEpisode' }),
   host: DS.belongsTo('partner', { inverse: 'hostEpisodes' })
 });

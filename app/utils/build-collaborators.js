7b0d18108d6ee6702727bdd0902ea7bbb8bf85eb
Jamie White <jamie@jgwhite.co.uk>
2014-11-24 14:58:02 +0000

    Avoid mutating episodes in buildCollaborators

--- app/utils/build-collaborators.js
+++ app/utils/build-collaborators.js
@@ -1,5 +1,9 @@
+import Ember from 'ember';
+
 export default function buildCollaborators(episodes) {
   return episodes.reduce(function(result, episode) {
+    episode = Ember.copy(episode);
+
     var prequel = episodes.findBy('guest_id', episode.host_id);
     if (prequel) { episode.prequel_id = prequel.id; }
 

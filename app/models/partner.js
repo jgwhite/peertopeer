bbfc0e84aac85abaceef651355d96a235822c831
Jamie White <jamie@jgwhite.co.uk>
2014-11-24 15:40:53 +0000

    Add inverses to partner

--- app/models/partner.js
+++ app/models/partner.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  guestEpisode: DS.belongsTo('episode'),
-  hostEpisodes: DS.hasMany('episode')
+  guestEpisode: DS.belongsTo('episode', { inverse: 'guest' }),
+  hostEpisodes: DS.hasMany('episode', { inverse: 'host' })
 });

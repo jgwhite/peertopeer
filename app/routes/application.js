89ced77920b0c3941510e66bcc48da0f3a3a3401
Jamie White <jamie@jgwhite.co.uk>
2014-11-24 15:13:47 +0000

    Use buildCollaborators and emit episode links

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 import ajax from 'ic-ajax';
+import buildCollaborators from '../utils/build-collaborators';
 
 export default Ember.Route.extend({
   model: function() {
-    return ajax('/api/episodes.json');
+    return ajax('/api/episodes.json').then(function(data) {
+      var episodes = data.episodes;
+      return buildCollaborators(episodes);
+    });
   }
 });

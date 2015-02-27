42dd9464fd7f83aed1bf1677da46babc33e47a3f
Jamie White <jamie@jgwhite.co.uk>
2015-02-27 20:27:15 +0000

    Refactor model loading in routes

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,10 +2,15 @@ import Ember from 'ember';
 let Promise = Ember.RSVP.Promise;
 
 export default Ember.Route.extend({
-  beforeModel: function() {
+  model: function() {
+    // Preload both datasets simultaneously
+    // because we don’t have /partners/:id
+    // endpoints to talk to.
     return Promise.all([
       this.store.find('episode'),
       this.store.find('partner')
-    ]);
+    ]).then(function([episodes]) {
+      return episodes;
+    });
   }
 });

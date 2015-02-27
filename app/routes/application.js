ae56cdc903f262c959e5662d25efe208b05eb2c6
Jamie White <jamie@jgwhite.co.uk>
2015-02-27 19:30:55 +0000

    Simplify routes

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 var RSVP = Ember.RSVP;
 
 export default Ember.Route.extend({
-  model: function() {
-    return RSVP.hash({
-      episodes: this.store.find('episode'),
-      partners: this.store.find('partner')
-    });
-  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    this.store.all('episode').mapBy('prequels');
-  },
-
   model: function() {
-    return this.store.filter('episode', function(episode) {
-      return episode.get('prequels.length') === 0;
-    });
+    return this.store.find('episode');
   }
 });
